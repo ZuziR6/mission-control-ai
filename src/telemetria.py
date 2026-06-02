b34d18f6b27b583fc4379b3bb946df5dd49e6e2f
ZuziR6 <gabrielgoliver1@gmail.com>
2026-06-02 13:47:18 -0300

    Update telemetria.py

--- src/telemetria.py
+++ src/telemetria.py
@@ -1,1 +1,19 @@
+"""
+Geração de telemetria simulada da missão EnviroSat.
+"""
 
+import random
+
+
+def coletar():
+    """
+    Retorna um conjunto de dados simulados da missão.
+    """
+
+    return {
+        "energia": random.randint(10, 100),                 # %
+        "temperatura": random.randint(20, 90),             # °C
+        "sensor_termico": random.randint(50, 100),         # %
+        "buffer_imagens": random.randint(0, 100),          # %
+        "precisao_gps": round(random.uniform(0.5, 8.0), 2) # metros
+    }
